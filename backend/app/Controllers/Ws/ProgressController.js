e86cc57b57623ce44ac0a61b04163bf674939b07
Patrick Dronk <patrickdronk@me.com>
2018-08-18 15:32:32 +0200

    m

--- backend/app/Controllers/Ws/ProgressController.js
+++ backend/app/Controllers/Ws/ProgressController.js
@@ -4,17 +4,19 @@ const Event = use('Event');
 class ProgressController {
   constructor({socket}) {
     this.socket = socket;
-    Event.on('torrent.progress', (info) => {
-      try {
-        this.socket.broadcastToAll('torrent.progress', info);
-      } catch(e) {}
-    });
+    // Event.on('torrent.progress', (info) => {
+    //   try {
+    //     this.socket.broadcastToAll('torrent.progress', info);
+    //   } catch(e) {}
+    // });
 
     Event.on('torrent.added', (info) => {
       console.log('progressController torrent.added');
       try {
         this.socket.broadcastToAll('torrent.added', info)
-      } catch(e) {}
+      } catch(e) {
+        console.log(e);
+      }
     });
   }
 }
